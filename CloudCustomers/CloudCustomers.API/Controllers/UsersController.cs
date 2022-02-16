e02cad54adff5933aace550cb5421e79ea5ea7d7
Antonio <antoniotmll@gmail.com>
2022-02-16 22:38:01 +0100

    Users controller fixed

---
+++ CloudCustomers/CloudCustomers.API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly ILogger<UsersController> _logger;
+
+    public UsersController(ILogger<UsersController> logger)
+    {
+        _logger = logger;
+    }
+        
+    [HttpGet(Name = "GetUsers")]
+    public async Task<IActionResult> Get()
+    {
+        return null;
+    }
+}
