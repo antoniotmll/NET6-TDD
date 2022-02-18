514e49ee2d02dac7e3945508564d039e5c3d7005
Antonio <antoniotmll@gmail.com>
2022-02-18 21:33:36 +0100

    Added address and user models

---
+++ CloudCustomers/CloudCustomers.API/Models/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudCustomers.API.Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
