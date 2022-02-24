b14d24d288b063d9d071a18599dcbb2e71cc67d3
Antonio <antoniotmll@gmail.com>
2022-02-24 22:25:08 +0100

    Added testusersservice

---
+++ CloudCustomers/CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace CloudCustomers.UnitTests.Systems.Services
+{
+    public class TestUsersService
+    {
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_InvokesHttpGetRequest()
+        {
+            var sut = new UsersService();
+
+            await sut.GetAllUsers();
+        }
+        
+    }
+}
