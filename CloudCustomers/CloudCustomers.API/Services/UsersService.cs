7c1df5f5b66686eccb14bb1012de88901cb6f052
Antonio <antoniotmll@gmail.com>
2022-02-25 18:19:55 +0100

    Added constructor

--- CloudCustomers/CloudCustomers.API/Services/UsersService.cs
+++ CloudCustomers/CloudCustomers.API/Services/UsersService.cs
@@ -3,6 +3,13 @@ using System;
 
 public class UsersService : IUsersService
 {
+    private readonly HttpClient _httpClient;
+
+    public UsersService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
     public Task<List<User>> GetAllUsers()
     {
         throw new NotImplementedException();
