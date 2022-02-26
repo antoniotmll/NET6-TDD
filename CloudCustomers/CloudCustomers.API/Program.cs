31afecfb7525ecb28bd013f4acc41a378d081881
Antonio <antoniotmll@gmail.com>
2022-02-26 18:50:09 +0100

    Added httclient in program.cs

--- CloudCustomers/CloudCustomers.API/Program.cs
+++ CloudCustomers/CloudCustomers.API/Program.cs
@@ -29,4 +29,5 @@ app.Run();
 void ConfigureServices(IServiceCollection services)
 {
     services.AddTransient<IUsersService, UsersService>();
+    services.AddHttpClient<IUsersService, UsersService>();
 }
