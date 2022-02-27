38074696c243e100fe66f9a1939dd0a37f359074
Antonio <antoniotmll@gmail.com>
2022-02-27 22:02:09 +0100

    Added handler

---
+++ CloudCustomers/CloudCustomers.UnitTests/Helpers/MockHttpMessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloudCustomers.UnitTests.Helpers
+{
+    internal static class MockHttpMessageHandler<T>
+    {
+        internal static Mock<HttpMessageHandler> SetupBasicGetResourceList(List<T> expectedResponse)
+        {
+            var mockResponse = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(expectedResponse))
+            };
+
+            mockResponse.Content.Headers.ContentType = 
+                new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var handlerMock = new Mock<HttpMessageHandler>();
+
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(mockResponse);
+
+            return handlerMock; 
+                        
+        }
+    }
+}
