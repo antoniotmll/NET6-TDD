edb14ed96e5c875df6fa9e64741788be47072561
Antonio <antoniotmll@gmail.com>
2022-02-28 22:23:33 +0100

    Net method added

--- CloudCustomers/CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
+++ CloudCustomers/CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -1,4 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using CloudCustomers.API.Models;
+using CloudCustomers.UnitTests.Helpers;
+using Moq;
+using Moq.Protected;
 using Xunit;
 
 namespace CloudCustomers.UnitTests.Systems.Services
@@ -8,9 +14,20 @@ namespace CloudCustomers.UnitTests.Systems.Services
         [Fact]
         public async Task GetAllUsers_WhenCalled_InvokesHttpGetRequest()
         {
-            var sut = new UsersService();
+            var expectedResponse = UserFixture.GetTestUsers();
+            var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var sut = new UsersService(httpClient);
 
             await sut.GetAllUsers();
+
+            handlerMock
+                .Protected()
+                .Verify(
+                "SendAsync", 
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get)),
+                ItExpr.IsAny<CancellationToken>();
         }
         
     }
