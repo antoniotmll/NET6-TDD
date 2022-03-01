9f3a4be1ee0d55736bbf97c5a47b6d86cf6433cb
Antonio <antoniotmll@gmail.com>
2022-03-01 21:30:11 +0100

    Fixed dependency

--- CloudCustomers/CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
+++ CloudCustomers/CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using CloudCustomers.API.Models;
+using CloudCustomers.UnitTests.Fixtures;
 using CloudCustomers.UnitTests.Helpers;
 using Moq;
 using Moq.Protected;
@@ -14,7 +15,7 @@ namespace CloudCustomers.UnitTests.Systems.Services
         [Fact]
         public async Task GetAllUsers_WhenCalled_InvokesHttpGetRequest()
         {
-            var expectedResponse = UserFixture.GetTestUsers();
+            var expectedResponse = UsersFixture.GetTestUsers();
             var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
             var httpClient = new HttpClient(handlerMock.Object);
             var sut = new UsersService(httpClient);
