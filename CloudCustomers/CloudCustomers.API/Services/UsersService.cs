aeb191af2225b9adfad20eba250a061e873e7163
Antonio <antoniotmll@gmail.com>
2022-03-04 16:58:39 +0100

    getallusers method added

--- CloudCustomers/CloudCustomers.API/Services/UsersService.cs
+++ CloudCustomers/CloudCustomers.API/Services/UsersService.cs
@@ -13,6 +13,12 @@ public class UsersService : IUsersService
     public async Task<List<User>> GetAllUsers()
     {
         var usersResponse = await _httpClient.GetAsync("https://example.com");
-        return new List<User> { };
+        if (usersResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+        {
+            return new List<User>();
+        }
+        var responseContent = usersResponse.Content;
+        var allUsers = await responseContent.ReadFromJsonAsync<List<User>>();
+        return allUsers.ToList();
     }
 }
